260c71b23e9aec34f08c9807ebd9f9ee53591219
Andrew Hill <andrew@textile.io>
2023-01-03 12:48:54 -0800

    update(dapp): push nft to web3.storage
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- contracts/GameLevels.sol
+++ contracts/GameLevels.sol
@@ -315,7 +315,7 @@ contract GameLevels is
                 string.concat(
                     '{"name":"',
                     word,
-                    '","animation_url":"https://bafybeicumkx2qamzsednvelo7kcuauhpu7kjjgx6mvck5ejacuh2iw3x2y.ipfs.cf-ipfs.com/?table=',stored._gameStoreName,'&token=',
+                    '","animation_url":"https://bafybeicdh35zz4lrcfrkovuoicfjkwmbqdw5duvqxrjy7mhvdqa6panovq.ipfs.w3s.link/?table=',stored._gameStoreName,'&token=',
                     strTokenId,
                     '","image":"',
                     image,
